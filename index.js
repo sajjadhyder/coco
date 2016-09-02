7f976953a1d88497a7ebef22825bc5591ba391b9
sajjadhyder <mr.sajjad.hyder@gmail.com>
2016-09-02 19:46:47 +0530

    Fixed error

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
+const bodyParser= require('body-parser');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+app.use(bodyParser.urlencoded({extended: true}));
+
+//--------database---------------
 var mongodb = require('mongodb');
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
@@ -17,30 +21,15 @@ MongoClient.connect(url, function (err, db) {
   } else {
     //HURRAY!! We are connected. :)
     console.log('Connection established to', url);
-    console.log('baatabase is', db);
-
-
     // do some work here with the database.
-    	var db = 
-        var collection = db.collection('users');
-
-    //Create some users
-    var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
-    var user2 = {name: 'modulus user', age: 22, roles: ['user']};
-    var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
-
-    // Insert some users
-    collection.insert([user1, user2, user3], function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-      }
-
+            
+            
+        }   
+     
     //Close connection
     db.close();
-  }
-});
+  });
+//--------database---------------
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -63,11 +52,59 @@ app.set('view engine', 'ejs');
 app.get('/index2', function(request, response) {
   response.render('pages/index2');
 });
+app.get('/thanks', function(request, response) {
+  response.render('pages/thanks');
+});
+app.get('/login', function(request, response) {
+  response.render('pages/login');
+});
 app.get('/product', function(request, response) {
   response.render('pages/product');
 });
 app.get('/contact', function(request, response) {
   response.render('pages/contact');
+});
+app.post('/contact-form', (req, res) => {
+	//--------database---------------
+	var mongodb = require('mongodb');
+
+	//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+	var MongoClient = mongodb.MongoClient;
+
+	// Connection URL. This is where your mongodb server is running.
+	var url = 'mongodb://heroku_n9kwrtp6:1k04jvroh2k30rum37bt57l4ec@ds019950.mlab.com:19950/heroku_n9kwrtp6';
+
+	// Use connect method to connect to the Server
+	MongoClient.connect(url, function (err, db){
+	  if (err) {
+	    console.log('Unable to connect to the mongoDB server. Error:', err);
+	  } else {
+    // do some work here with the database.
+     	db.collection('cocofino', function (err, collection) {
+
+     	collection.insert({ Name: req.body.name, Email: req.body.email, Mobile: req.body.mobile, message: req.body.message });
+        });   
+
+     }
+    //Close connection
+    db.close();
+  });
+	
+	res.redirect('/thanks');
+
+//--------database---------------
+});
+
+app.post('/login', (req, res) => {
+	var Name= req.body.name;
+	var pass = req.body.password;
+ 	if (Name=='admin' && pass=='root' ){
+		res.redirect('/list');
+ 	}else{
+ 		res.send("Invalid data");
+ 	}
+
+//--------database---------------
 });
 
 app.listen(app.get('port'), function() {
